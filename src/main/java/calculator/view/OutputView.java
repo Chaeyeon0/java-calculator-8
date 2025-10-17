d8c419e7f43f2b9fc871ac064474d839d9263ee4
qorcodusl <qorcodus03@naver.com>
2025-10-17 23:56:43 +0900

    feat(view): OutputView 클래스 추가

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+public class OutputView {
+    public static void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public static void printError(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+}
