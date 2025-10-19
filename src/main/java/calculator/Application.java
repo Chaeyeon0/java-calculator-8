9924b299d4c21f8fcf2417e1c4c0d735de30adae
qorcodusl <qorcodus03@naver.com>
2025-10-19 21:25:38 +0900

    refactor(domain): Application에서 Controller 실행 구조로 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,23 +1,9 @@
 package calculator;
 
-import calculator.domain.StringCalculator;
-import calculator.domain.PositiveNumbers;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import calculator.controller.CalculatorController;
 
 public class Application {
     public static void main(String[] args) {
-        //기본 입출력
-        try{
-            System.out.println("덧셈할 문자열을 입력해 주세요.");
-            String inputLine = readLine();
-
-            StringCalculator calculator = new StringCalculator();
-            int result = calculator.add(inputLine);
-
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        new CalculatorController().run();
     }
 }
--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -5,6 +5,7 @@ public class StringCalculator {
     private final ExpressionParser parser = new ExpressionParser();
 
     public int add(String inputLine) {
+
         String[] numbers = parser.parse(inputLine);
         PositiveNumbers positiveNumbers = new PositiveNumbers(numbers);
         return positiveNumbers.sum();
