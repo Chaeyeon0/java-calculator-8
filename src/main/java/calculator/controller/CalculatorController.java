16742eebda3d4b6bef20a76dfbb7634de6f69fce
qorcodusl <qorcodus03@naver.com>
2025-10-19 21:23:00 +0900

    feat(controller): CalculatorController 클래스 추가 및 실행 흐름 제어

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,20 @@
+package calculator.controller;
+
+import calculator.domain.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+
+    private final StringCalculator calculator = new StringCalculator();
+
+    public void run() {
+        try {
+            String input = InputView.readInput();
+            int result = calculator.add(input);
+            OutputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+        }
+    }
+}
